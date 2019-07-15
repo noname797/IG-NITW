e821260d587b113b70d53265ba6f94233fd6aa43
noname797 <38657909+noname797@users.noreply.github.com>
2019-07-16 01:34:40 +0530

    Update CIFAR100.py

--- CIFAR100.py
+++ CIFAR100.py
@@ -6,31 +6,25 @@ x_train=x_train/255
 x_test=x_test/255
 from keras.layers import Conv2D,MaxPooling2D,Flatten,Dense,LeakyReLU,Dropout
 from keras.models import Sequential
-
+lr=LeakyReLU()
 classifier=Sequential()
 
-classifier.add(Conv2D(128,(3,3),activation='relu',input_shape=(32,32,3)))
+classifier.add(Conv2D(128,(3,3),activation=lr,input_shape=(32,32,3)))
 classifier.add(MaxPooling2D(pool_size=(2,2)))
-classifier.add(Dropout(rate=0.25))
-
-classifier.add(Conv2D(64,(3,3),activation='relu'))
+classifier.add(Dropout(0.4))
+classifier.add(Conv2D(128,(3,3),activation=lr,input_shape=(32,32,3)))
 classifier.add(MaxPooling2D(pool_size=(2,2)))
-classifier.add(Dropout(rate=0.25))
-
-classifier.add(Conv2D(32,(3,3),activation='relu'))
-classifier.add(MaxPooling2D(pool_size=(2,2)))
-classifier.add(Dropout(rate=0.25))
-
+classifier.add(Dropout(0.4))
 classifier.add(Flatten())
 
-classifier.add(Dense(512,activation='relu'))
-classifier.add(Dropout(0.3))
-classifier.add(Dense(512,activation='relu'))
-classifier.add(Dropout(0.3))
+classifier.add(Dense(512,activation=lr))
+classifier.add(Dropout(0.5))
+classifier.add(Dense(512,activation=lr))
+classifier.add(Dropout(0.5))
 classifier.add(Dense(100,activation='softmax'))
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9)
 classifier.compile(optimizer=sgd,loss='sparse_categorical_crossentropy',metrics=['accuracy'])
 
-classifier.fit(x_train,y_train,epochs = 100,batch_size=128,validation_data=(x_test,y_test))
+classifier.fit(x_train,y_train,epochs = 100,batch_size=64,validation_data=(x_test,y_test))
 
 #explaination on https://docs.google.com/document/d/17NtDAWE1VZDkLKimjRyGgyuj4NXDuG-eCXspgv6sD3E/edit?usp=sharing
