c240d2bce9abecc6c422dc25b47e995f5406065b
noname797 <noname797@localhost.localdomain>
2020-01-12 22:05:55 -0500

    alpha 1.5

--- weather/weather_p1.py
+++ weather/weather_p1.py
@@ -46,7 +46,6 @@ x_train_rnn=[]
 y_train_rnn=[]
 
 
-#try stacking y_train_rnn and then supply to the network probably will work
 
 
 for i in range(120,3943-120):
@@ -59,10 +58,10 @@ x_train_rnn=x_train_rnn[:,:,1:].astype("float32")
 y_train_rnn=y_train_rnn[:,:,1:].astype("float32")
 
 a=x_train_rnn.reshape(3703,120*21)
-
+'''
 from sklearn.preprocessing import StandardScaler
 sc_0=StandardScaler()
-x_train_rnn=sc_0.fit_transform(a,y_train_rnn).reshape(3703,120,21)
+x_train_rnn=sc_0.fit_transform(a,y_train_rnn).reshape(3703,120,21)'''
 
 
 
@@ -73,12 +72,12 @@ def next_batch(batch_size):
     return x_batch,y_batch
 
 
-#LSTM
+#LSTM considering past 120 days
+    batch_size=64
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense,LSTM,Dropout,BatchNormalization
 initializer=tf.keras.initializers.GlorotNormal
-"""
 model0=Sequential()
 model0.add(Dense(128,activation='relu',input_shape=(x_train_rnn.shape[1],x_train_rnn.shape[2]),kernel_initializer=initializer))
 model0.add(Dropout(0.1))
@@ -94,16 +93,13 @@ model0.add(BatchNormalization())
 #model0.add(Dense(512,activation='relu',kernel_initializer=initializer))
 #model0.add(Dropout(0.1))
 #model0.add(BatchNormalization())
-model0.add(Dense(157))
+model0.add(Dense(147))
 #model0.add(BatchNormalization())
-x_batch,y_batch=next_batch(64)
-
 model0.compile(optimizer='rmsprop',loss='mse',metrics=['accuracy'])
-model0.fit(x_batch,y_batch,epochs=5,batch_size=64)"""
-
+model0.fit(x_train_rnn,y_train_rnn.reshape(3703,7*21),epochs=50,batch_size=64)
+'''
 x=tf.compat.v1.placeholder(tf.float32,shape=[None,x_train_rnn.shape[1],x_train_rnn.shape[2]])
 y=tf.compat.v1.placeholder(tf.float32,shape=[None,y_train_rnn.shape[1]*y_train_rnn.shape[2]])
-'''
 def model_inputs():
     """
     Create the model inputs
@@ -215,11 +211,11 @@ svm_data_processed=svm_data_processed.reshape(293*11,8)
 svm_data_processed_2=svm_data_processed_2.reshape(72*10,8)
 svm_data_with_direction=np.append(svm_data_processed,svm_data_processed_2,axis=0)
 svm_data_with_direction=np.append(svm_data_with_direction,list2,axis=1)
-
+'''
 #normalising
 from sklearn.preprocessing import StandardScaler
 sc_1=StandardScaler()
-svm_data_with_direction=sc_1.fit_transform(svm_data_with_direction)
+svm_data_with_direction=sc_1.fit_transform(svm_data_with_direction)'''
 
 
 svm_data_processed=svm_data_with_direction[0:293*11,:].reshape(293,11,22)
@@ -239,32 +235,32 @@ y_train_2_p2=svm_data_processed_2[:,0,1:].reshape(72,1,21)
 
 
 
-#lstm year wise
+#lstm considering last 7 years
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense,LSTM,Dropout,BatchNormalization
 initializer=tf.keras.initializers.GlorotNormal
-
+y_train_1_p1=y_train_1_p1.reshape(293,21)
 
 model1=Sequential()
 model1.add(Dense(128,activation='relu',input_shape=(x_train_1_p1.shape[1],x_train_1_p1.shape[2]),kernel_initializer=initializer))
 model1.add(Dropout(0.1))
 model1.add(LSTM(512,kernel_initializer=initializer,dropout=0.15,recurrent_dropout=0.15,return_sequences=True,go_backwards=True))
 model1.add(BatchNormalization())
-#model1.add(LSTM(1024,kernel_initializer=initializer,dropout=0.15,recurrent_dropout=0.15,return_sequences=True,go_backwards=True))
-#model1.add(BatchNormalization())
+model1.add(LSTM(1024,kernel_initializer=initializer,dropout=0.15,recurrent_dropout=0.15,return_sequences=True,go_backwards=True))
+model1.add(BatchNormalization())
 model1.add(LSTM(512,kernel_initializer=initializer,dropout=0.15,recurrent_dropout=0.15,go_backwards=True))
 model1.add(BatchNormalization())
-#model1.add(Dense(1024,activation='relu',kernel_initializer=initializer))
-#model1.add(Dropout(0.1))
-#model1.add(BatchNormalization())
-#model1.add(Dense(512,activation='relu',kernel_initializer=initializer))
-#model1.add(Dropout(0.1))
-#model1.add(BatchNormalization())
+model1.add(Dense(1024,activation='relu',kernel_initializer=initializer))
+model1.add(Dropout(0.1))
+model1.add(BatchNormalization())
+model1.add(Dense(512,activation='relu',kernel_initializer=initializer))
+model1.add(Dropout(0.1))
+model1.add(BatchNormalization())
 model1.add(Dense(21))
-#model1.add(BatchNormalization())
 
-model1.compile(optimizer='rmsprop',loss='mse',metrics=['accuracy'])
-model1.fit(x_train_1_p1,y_train_1_p1.reshape(293,21),epochs=5,batch_size=64)
+
+model1.compile(optimizer='adam',loss='mse',metrics=['accuracy'])
+model1.fit(x_train_1_p1,y_train_1_p1,epochs=50,batch_size=32)
 
 
 model2=Sequential()
@@ -272,44 +268,50 @@ model2.add(Dense(128,activation='relu',input_shape=(x_train_2_p1.shape[1],x_trai
 model2.add(Dropout(0.1))
 model2.add(LSTM(512,kernel_initializer=initializer,dropout=0.15,recurrent_dropout=0.15,return_sequences=True,go_backwards=True))
 model2.add(BatchNormalization())
-#model2.add(LSTM(1024,kernel_initializer=initializer,dropout=0.15,recurrent_dropout=0.15,return_sequences=True,go_backwards=True))
-#model2.add(BatchNormalization())
+model2.add(LSTM(1024,kernel_initializer=initializer,dropout=0.15,recurrent_dropout=0.15,return_sequences=True,go_backwards=True))
+model2.add(BatchNormalization())
 model2.add(LSTM(512,kernel_initializer=initializer,dropout=0.15,recurrent_dropout=0.15,go_backwards=True))
 model2.add(BatchNormalization())
-#model2.add(Dense(1024,activation='relu',kernel_initializer=initializer))
-#model2.add(Dropout(0.1))
-#model2.add(BatchNormalization())
-#model2.add(Dense(512,activation='relu',kernel_initializer=initializer))
-#model2.add(Dropout(0.1))
-#model2.add(BatchNormalization())
+model2.add(Dense(1024,activation='relu',kernel_initializer=initializer))
+model2.add(Dropout(0.1))
+model2.add(BatchNormalization())
+model2.add(Dense(512,activation='relu',kernel_initializer=initializer))
+model2.add(Dropout(0.1))
+model2.add(BatchNormalization())
 model2.add(Dense(21))
-#model2.add(BatchNormalization())
-
-model2.compile(optimizer='rmsprop',loss='mse',metrics=['accuracy'])
-model2.fit(x_train_2_p1,y_train_2_p1.reshape(72,21),epochs=5,batch_size=64)
 
 
+model2.compile(optimizer='adam',loss='mse',metrics=['accuracy'])
+model2.fit(x_train_2_p1,y_train_2_p1.reshape(72,21),epochs=200,batch_size=32)
+
+y_pred_1_p2_lstm=model1.predict(x_train_1_p2)
+y_pred_2_p2_lstm=model2.predict(x_train_2_p2)
+#is lstm running fine?
+y_train_1_p2=y_train_1_p2.reshape(293,21)
 #for loop and apply svm and append the results into list from p1
 from sklearn.ensemble import RandomForestRegressor
-y_pred_p1=[]
-y_pred_p2=[]
+y_pred_1_p2=[]
+y_pred_2_p2=[]
 for i in range(len(x_train_1_p1)):
     model_rf=RandomForestRegressor(n_estimators=500)
     #model_rf.fit(x_train_1_p1[0].T,y_train_1_p1[0].T)
     model_rf.fit(x_train_1_p1[i].T,y_train_1_p1[i].T)#doubt
-    y_pred_p1.append(model_rf.predict(x_train_1_p2[i].T))#doubt (Gives better results for some)
+    y_pred_1_p2.append(model_rf.predict(x_train_1_p2[i].T))#doubt (Gives better results for some)
     #model_rf.predict(x_train_1_p1[1].T)
 for i in range(len(x_train_2_p1)):
     model_rf=RandomForestRegressor(n_estimators=500)
     #model_rf.fit(x_train_2_p1[0].T,y_train_2_p1[0].T)
     model_rf.fit(x_train_2_p1[i].T,y_train_2_p1[i].T)#doubt
-    y_pred_p2.append(model_rf.predict(x_train_2_p2[i].T))#doubt (Gives better results for some)
+    y_pred_2_p2.append(model_rf.predict(x_train_2_p2[i].T))#doubt (Gives better results for some)
     #model_rf.predict(x_train_2_p1[1].T)
 
-y_pred_p1=np.array(y_pred_p1)
-y_pred_p1=y_pred_p1.reshape(293,1,21)
-y_pred_p2=np.array(y_pred_p2)
-y_pred_p2=y_pred_p2.reshape(72,1,21)
+y_pred_1_p1=np.array(y_pred_1_p1)
+y_pred_1_p1=y_pred_1_p1.reshape(293,1,21)
+y_pred_2_p1=np.array(y_pred_2_p1)
+y_pred_2_p1=y_pred_2_p1.reshape(72,1,21)
+
+
+
 import csv
 
 with open('filename', 'w') as myfile:
